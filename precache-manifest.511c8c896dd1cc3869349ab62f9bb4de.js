ad46fa59a4dac5d1eab59c94839879984233ec1c
michaellorzing <mike.l@skunexus.com>
2019-11-26 19:55:07 -0500

    Updates

---
+++ precache-manifest.511c8c896dd1cc3869349ab62f9bb4de.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5488db40b30f778f9bc48f925a70022",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "32c93409aa59c0322af5",
+    "url": "/portfolio/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "9aeb451f6c168d7fd89a",
+    "url": "/portfolio/static/css/main.ad6a93c8.chunk.css"
+  },
+  {
+    "revision": "32c93409aa59c0322af5",
+    "url": "/portfolio/static/js/2.2f0bfcba.chunk.js"
+  },
+  {
+    "revision": "9aeb451f6c168d7fd89a",
+    "url": "/portfolio/static/js/main.8e8526f7.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  },
+  {
+    "revision": "c865c81727ae22d15a8de6e4f3fcc1a1",
+    "url": "/portfolio/static/media/crystal.c865c817.png"
+  },
+  {
+    "revision": "e34df7b8e3bdb3d88a0cdee842ac2d67",
+    "url": "/portfolio/static/media/me2.e34df7b8.png"
+  },
+  {
+    "revision": "71ad1d973ec6172284c6cefb9b55e3bc",
+    "url": "/portfolio/static/media/moveimate.71ad1d97.png"
+  },
+  {
+    "revision": "a72f2823f43517e577934b7ac31f8af0",
+    "url": "/portfolio/static/media/moviematereact.a72f2823.png"
+  },
+  {
+    "revision": "f516596acf1311cb1205bbf986230a8d",
+    "url": "/portfolio/static/media/notetaker.f516596a.png"
+  },
+  {
+    "revision": "73cdadd3404ab85c68a8e29f6a756ac2",
+    "url": "/portfolio/static/media/skunexus.73cdadd3.png"
+  },
+  {
+    "revision": "a87284f6ee4233b3e4e792cf15f2eb81",
+    "url": "/portfolio/static/media/spacex.a87284f6.png"
+  }
+]);
